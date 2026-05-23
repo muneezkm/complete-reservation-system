5da0b96711cb2d798bd3f721ce70cdd3b8a25ace
Muneez <munizmunzz@gmail.com>
2026-05-24 03:04:07 +0530

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,3 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals.js"),
-];
-
-export default eslintConfig;
+/** @type {import('eslint').Linter.Config[]} */
+const config = [];
+export default config;
